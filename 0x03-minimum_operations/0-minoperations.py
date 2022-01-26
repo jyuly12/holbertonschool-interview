cf0edb9c4506a13f57154aeb1cf895697460aacd
jyuly12 <yuly.gonzalez2021@gmail.com>
2022-01-26 00:23:51 -0500

    Correct file

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -9,14 +9,14 @@ def minOperations(n):
         This code calculates the number of operations (copy and paste)
         that must be carried out to obtain the given number
     """
-    number = 2
-    count = 0
-    if n == 0:
+    if n <= 1:
         return 0
-    while (count <= n):
-        if (n % number):
-            n = int(n / number)
-            count += number
-            number = 1
-        count += 1
-    return (count)
+    num, div, numOfOperations = n, 2, 0
+
+    while num > 1:
+        if num % div == 0:
+            num = num / div
+            numOfOperations = numOfOperations + div
+        else:
+            div += 1
+    return numOfOperations

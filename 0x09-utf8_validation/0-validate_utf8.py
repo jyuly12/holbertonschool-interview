d6e8f8989d398896b2252d574b8d6b40082692e9
jyuly12 <yuly.gonzalez2021@gmail.com>
2022-02-17 22:46:34 -0500

    Add new task

---
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+""" This module defines if data set represents a valid UTF-8 encoding.
+"""
+import base64
+
+
+def validUTF8(data):
+    """ Validator function
+    """
+    bit1 = 1 << 7
+    bit2 = 1 << 6
+    nbytes = 0
+
+    if not data or len(data) == 0:
+        return True
+
+    for num in data:
+        bit = 1 << 7
+        if nbytes == 0:
+            while (bit & num):
+                nbytes += 1
+                bit = bit >> 1
+
+            if nbytes == 0:
+                continue
+            if nbytes == 1 or nbytes > 4:
+                return False
+        else:
+
+            if not (num & bit1 and not (num & bit2)):
+                return False
+        nbytes -= 1
+
+    if nbytes:
+        return False
+    else:
+        return True

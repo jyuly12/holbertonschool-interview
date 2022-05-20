3f9596839ef681bc8e9f70ad0b66b53146ff47a3
jyuly12 <yuly.gonzalez2021@gmail.com>
2022-05-19 21:22:59 -0500

    Add files

---
+++ 0x15-infinite_multiplication/holberton.h
@@ -0,0 +1,15 @@
+#ifndef _LIST_H
+#define _LIST_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+
+int _putchar(char c);
+void _return_error(void);
+int only_digits(char *num);
+int _multiply(char *num1, unsigned int len1, char *num2, unsigned int len2);
+
+
+#endif

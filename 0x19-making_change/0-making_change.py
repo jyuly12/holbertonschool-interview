3255eee65130f9006c1192a4e831b3e5c2b11188
jyuly12 <yuly.gonzalez2021@gmail.com>
2022-06-23 18:32:22 -0500

    Add task files

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -8,7 +8,7 @@ def makeChange(coins, total):
     if (total <= 0):
         return 0
 
-    array = [0] * (total + 1)     
+    array = [0] * (total + 1)
     for i in range(1, total + 1):
         array[i] = sys.maxsize
         for j in range(len(coins)):
